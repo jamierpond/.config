c8b47d6ea47785e37b05c4923658a0e5961ad197
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-10-29 14:15:33 -0700

    not silent

--- nvim/lua/theprimeagen/remap.lua
+++ nvim/lua/theprimeagen/remap.lua
@@ -40,6 +40,6 @@ vim.api.nvim_set_keymap("n", "<leader><leader>", ":so<CR>", {})
 
 vim.api.nvim_set_keymap("v", "<leader>y", [["*y]], {})
 
-vim.cmd([[command! -nargs=+ Gc :silent !git commit -a -m "<args>"]])
+vim.cmd([[command! -nargs=+ Gc !git commit -a -m "<args>"]])
 
 
