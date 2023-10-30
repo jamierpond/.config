e4b995b9443559f13196259f190491f80a419bad
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2023-10-29 19:10:05 -0700

    this is fun

--- nvim/lua/theprimeagen/remap.lua
+++ nvim/lua/theprimeagen/remap.lua
@@ -45,6 +45,7 @@ vim.cmd([[command! Gp !git push]])
 vim.cmd([[command! Kat !killall tmux]])
 vim.cmd([[command! Kan !killall node]])
 
+-- more fun
 -- fun git stuff
 local function git_exec(message, additional_cmd)
   local cmd = "!git add ."
