142dacf74faf034311c8ccf136d182c90f2f4588
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-04-24 16:05:32 +0100

    update

--- nvim/lua/jamie/init.lua
+++ nvim/lua/jamie/init.lua
@@ -34,3 +34,10 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
+vim.cmd [[
+  augroup markdown_settings
+    autocmd!
+    autocmd FileType markdown setlocal textwidth=0 wrapmargin=0 wrap linebreak
+    autocmd FileType markdown setlocal columns=90
+  augroup END
+]]
