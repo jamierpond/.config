248c44834a3cd405379aa34961edc0a4f1f1936e
Jamie Pond <jamiepond259@gmail.com>
2024-06-10 22:55:21 +0000

    platform

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -7,8 +7,6 @@ alias bwoff="shortcuts run \"bw-off\""
 alias cpp-compile="~/.config/bin/scripts/cpp-compile"
 alias cppc="~/.config/bin/scripts/cpp-compile"
 
-alias so="source ~/.zshrc && source ~/.bashrc"
-
 alias "pipi"="pip install -r requirements.txt"
 
 
@@ -17,4 +15,16 @@ alias smu="git submodule update --init --recursive"
 
 alias "new-venv"="python3 -m venv venv"
 alias "act-venv"="source venv/bin/activate"
+
+if [ -f ~/.zshrc ]; then
+   alias so="source ~/.zshrc && source ~/.bashrc"
+else
+   echo "No zshrc file found"
+   alias so="source ~/.bashrc"
+fi
+
+
+
+
+
 
