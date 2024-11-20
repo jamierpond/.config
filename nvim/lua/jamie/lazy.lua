6a275e03eb6f0712e3061702e5a877f94b6ffd5d
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-11-20 15:07:01 -0800

    more

--- nvim/lua/jamie/lazy.lua
+++ nvim/lua/jamie/lazy.lua
@@ -16,6 +16,37 @@ local lazy = require("lazy")
 lazy.setup({
   {"nvim-lua/plenary.nvim"},
   {"lervag/vimtex"},
+
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      { "antosha417/nvim-lsp-file-operations", config = true },
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local util = require("lspconfig.util")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+      local capabilities = cmp_nvim_lsp.default_capabilities()
+      local opts = { noremap = true, silent = true }
+      local on_attach = function(_, bufnr)
+        opts.buffer = bufnr
+
+        opts.desc = "Show line diagnostics"
+        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+        opts.desc = "Show documentation for what is under cursor"
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      end
+
+      lspconfig["sourcekit"].setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+    end,
+  },
+
   {"nvim-telescope/telescope-live-grep-args.nvim"},
   {'mfussenegger/nvim-dap'},
 
