8ea74a84266dc78a6bf64e162a172f4aff805043
Jamie Pond <jamiepond259@gmail.com>
2024-12-04 19:36:34 +0000

    jamie

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', "<cmd>Telescope frecency workspace=CWD<CR>", { desc = "Search" })
--- vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- this is the og one
+-- vim.keymap.set('n', '<leader>pf', "<cmd>Telescope frecency workspace=CWD<CR>", { desc = "Search" })
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- this is the og one
 
 vim.keymap.set('n', '<C-g>', builtin.git_files, {})
 -- vim.keymap.set('n', '<leader>ps', function()
