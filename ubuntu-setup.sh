1effa78ce8ee6c20210a685233972ea3ea846d2c
Jamie Pond <jamiepond259@gmail.com>
2025-03-19 13:28:19 -0700

    wip

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -6,20 +6,19 @@ sudo apt update && sudo apt install -y build-essential nodejs npm unzip zip    \
   fzf ripgrep snapd ffmpeg sox libsox-dev pkg-config protobuf-compiler cmake   \
   linux-libc-dev clang git-secret libomp-dev ranger zsh -y
 
+sudo snap install docker go tmux nvim
+
+
+rm -rf ./lazygit-tmp
+mkdir -p ./lazygit-tmp
+cd ./lazygit-tmp
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+rm -rf lazygit.tar.gz lazygit
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
-
-# install docker
-# 1. add gpg key
-sudo snap install docker
-
-# # install rust
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-#
-# # go
-sudo snap install go --classic
+cd ..
+rm -rf ./lazygit-tmp
 
 # todo just make this using a token
 # install gh cli
@@ -45,13 +44,16 @@ git config --global user.email "jamiepond259@gmail.com"
 git config --global user.name "Jamie Pond"
 
 # TODO make this also copy the dotfiles (.git and .gitignore)
-rm -rf ~/jamie-config
-git clone https://github.com/jamierpond/.config ~/jamie-config
-cp -r ~/jamie-config/* ~/.config
-
-# install nvims
-sudo snap install tmux --classic
-sudo snap install nvim --classic
+cd "$HOME"
+# if the .config already exits then skip cloning it, else clone
+if [ ! -f ~/.config ]; then
+  echo "it doesn't already exist"
+  rm -rf ~/jamie-config
+  git clone https://github.com/jamierpond/.config ~/jamie-config
+  cp -r ~/jamie-config/* ~/.config
+else
+  echo "not recloning"
+fi
 
 # tmux alias, so we can use our config
 echo "alias tmux='tmux -f ~/.config/tmux/tmux.conf'" >> ~/.bashrc
