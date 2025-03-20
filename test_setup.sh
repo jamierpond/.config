460a754fdcd9ce6313eb7e94d103017eb9606fb1
Jamie Pond <jamiepond259@gmail.com>
2025-03-20 16:42:54 -0700

    ensure non-interactive

--- test_setup.sh
+++ test_setup.sh
@@ -1,5 +1,5 @@
 docker run --rm -it \
   -v "$(realpath ./ubuntu-setup.sh):/tmp/ubuntu-setup.sh" \
-  --entrypoint /bin/bash ubuntu:22.04 -c \
+  --entrypoint /bin/bash ubuntu:20.04 -c \
   "apt-get update && apt-get install -y curl sudo && chmod +x /tmp/ubuntu-setup.sh && /tmp/ubuntu-setup.sh"
 
