404f1f2645f9bce6319bb49febed2e471048f9ba
Jamie Pond <jamiepond259@gmail.com>
2025-03-20 15:25:21 -0700

    wip

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -15,8 +15,15 @@ rm -rf ./lazygit-tmp
 mkdir -p ./lazygit-tmp
 cd ./lazygit-tmp
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | jq -r .tag_name)
+LAZYGIT_URL="https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+echo "Downloading lazygit version ${LAZYGIT_VERSION}"
+echo "Downloading lazygit from ${LAZYGIT_URL}"
+
+# tmp
+exit 1
+
 rm -rf lazygit.tar.gz lazygit
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+curl -Lo lazygit.tar.gz ""
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 cd ..
