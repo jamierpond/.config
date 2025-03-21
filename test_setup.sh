ffcdc28b2119ba89329f5495b64d552eee806303
Jamie Pond <jamiepond259@gmail.com>
2025-03-20 19:57:43 -0700

    simplify

--- test_setup.sh
+++ test_setup.sh
@@ -3,14 +3,11 @@ if [ -z "$UBUNTU_VERSION" ]; then
   UBUNTU_VERSION="20.04"
 fi
 
-export TZ='America/Los_Angeles'
-
 docker run --rm -it \
    -v "$(pwd):/repo" \
    -e DEBIAN_FRONTEND=noninteractive \
    --entrypoint /bin/bash ubuntu:$UBUNTU_VERSION -c \
-   "DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y curl sudo && \
-   chmod +x /repo/ubuntu-setup.sh && \
-   DEBIAN_FRONTEND=noninteractive /repo/ubuntu-setup.sh && \
+   "apt-get update && apt-get install -y curl sudo && \
+   chmod +x /repo/ubuntu-setup.sh && /repo/ubuntu-setup.sh && \
    exec zsh -c 'source /repo/zshrc && echo \"✅ zshrc sourced!\" && exec zsh'"
 
