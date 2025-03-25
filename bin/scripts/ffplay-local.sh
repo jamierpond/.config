923cdf0a6fd9fa1c0df3bf40e83629acbaf82bc5
Jamie Pond <jamiepond259@gmail.com>
2025-03-25 01:16:06 -0700

    ok cool

--- bin/scripts/ffplay-local.sh
+++ bin/scripts/ffplay-local.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -e
+set -ex
 
 # copy a file from whever we are onto my local machine and play it >:)
+ssh_user="jamiepond"
+hostname="me.pond.audio"
 
 input_file="$1"
 if [ -z "$input_file" ]; then
@@ -18,14 +20,14 @@ sha=$(sha256sum $input_file | awk '{print $1}')
 trimmed_sha=$(echo $sha | cut -c1-8)
 extension="${input_file##*.}"
 
-ssh_user="jamiepond"
-ssh_host="$ssh_user@ssh.pond.audio"
+ssh_host="$ssh_user@$hostname"
 output_file="/Users/$ssh_user/.audiotmp/$trimmed_sha.$extension"
 
+
 function cp_file {
-  scp -o "StrictHostKeyChecking=accept-new" "$input_file" "$ssh_host:$output_file"
+  scp "$input_file" "$ssh_host:$output_file"
 }
 
 ssh -q "$ssh_host" [[ -f "$output_file" ]] && echo "File exists on host" || cp_file
 
-ssh -o "StrictHostKeyChecking=accept-new" "$ssh_host" -x "/opt/homebrew/bin/ffplay $output_file"
+ssh "$ssh_host" -x "/opt/homebrew/bin/ffplay $output_file"
