28f60b0c76439acbbc3a9818e43f61396a701748
Jamie Pond <jamiepond259@gmail.com>
2025-07-30 14:19:11 -0700

    wip

--- bin/scripts/clipimage.sh
+++ bin/scripts/clipimage.sh
@@ -2,19 +2,19 @@
 
 OUTFILE="./clipimage.png"
 
-# Use AppleScript to write PNG clipboard data to file
-osascript -e '
+osascript <<EOF
 try
-  set theImage to the clipboard as «class PNGf»
-  set outFile to POSIX file "'$OUTFILE'" as string
+  set pngData to the clipboard as «class PNGf»
+  set outFile to POSIX file "$(pwd)/clipimage.png"
   set fileRef to open for access outFile with write permission
   set eof fileRef to 0
-  write theImage to fileRef
+  write pngData to fileRef
   close access fileRef
 on error
   try
-    close access fileRef
+    close access outFile
   end try
-  error "No image data in clipboard"
-end try'
+  error "no image"
+end try
+EOF
 
